47a2c13f9c00b472ffc669f37aaf44a3ec6e7d27
tanova <ivana.tanova@tomtom.com>
2022-03-31 19:23:41 +0300

    Udacity Spring Web added

---
+++ Web Services & API/bootstrap/target/classes/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO location (id, name, address) VALUES (1, 'ATL Airport','Atlanta airport location');
+INSERT INTO location (id, name, address) VALUES (2, 'MIA Airport','Miami airport location');
+INSERT INTO location (id, name, address) VALUES (3, 'LAX Airport','Los Angeles airport location');
+INSERT INTO location (id, name, address) VALUES (4, 'PHX Airport','Phoenix airport location');
+INSERT INTO location (id, name, address) VALUES (5, 'ORD Airport','Chicago airport location');
+
+INSERT INTO dog (id, name, breed, origin) VALUES (1, 'Jo','Pomeran', 'Amsterdam');
+INSERT INTO dog (id, name, breed, origin) VALUES (2, 'Jo1','Husky', 'Amsterdam');
+INSERT INTO dog (id, name, breed, origin) VALUES (3, 'Jo2','Laika', 'Amsterdam');
+INSERT INTO dog (id, name, breed, origin) VALUES (4, 'Jo3','Jack', 'Amsterdam');
+INSERT INTO dog (id, name, breed, origin) VALUES (5, 'Jo4','Doberman', 'Amsterdam');
